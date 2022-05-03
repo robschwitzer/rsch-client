e7c32887580d5edde9162fb03fee335ac8adfeb6
rob <rob@sphere.guide>
2022-05-03 11:16:11 -0700

    Add redirect config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['res.cloudinary.com'] }
+  images: { domains: ['res.cloudinary.com'] },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/resume',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
